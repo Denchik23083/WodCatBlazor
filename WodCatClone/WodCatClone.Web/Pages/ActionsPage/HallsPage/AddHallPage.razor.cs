f4f1fe5d7b7d2828258a1eaff5244c8131f2d5dc
Denchik23083 <deniskudravov228@gmail.com>
2022-04-04 20:42:59 +0300

    template add hall

--- WodCatClone/WodCatClone.Web/Pages/ActionsPage/HallsPage/AddHallPage.razor.cs
+++ WodCatClone/WodCatClone.Web/Pages/ActionsPage/HallsPage/AddHallPage.razor.cs
@@ -1,6 +1,19 @@
-﻿namespace WodCatClone.Web.Pages.ActionsPage.HallsPage
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using WodCatClone.Db.Entities.Actions;
+using WodCatClone.Logic.ActionsService.WorkoutsService;
+
+namespace WodCatClone.Web.Pages.ActionsPage.HallsPage
 {
     public partial class AddHallPage
     {
+        [Inject] private IWorkoutsService _service { get; set; }
+
+        public IEnumerable<Workouts> Workouts { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Workouts = _service.GetAllWorkouts();
+        }
     }
 }
