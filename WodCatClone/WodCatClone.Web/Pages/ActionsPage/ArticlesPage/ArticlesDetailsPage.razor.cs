292a1ea09b59e6149751536d4f8621e905b90298
Denchik23083 <deniskudravov228@gmail.com>
2022-04-21 12:18:15 +0300

    Article by Id is done. Actions pages is done

--- WodCatClone/WodCatClone.Web/Pages/ActionsPage/ArticlesPage/ArticlesDetailsPage.razor.cs
+++ WodCatClone/WodCatClone.Web/Pages/ActionsPage/ArticlesPage/ArticlesDetailsPage.razor.cs
@@ -1,9 +1,23 @@
 ﻿using Microsoft.AspNetCore.Components;
+using WodCatClone.Db.Entities.Actions;
+using WodCatClone.Logic.ActionsService.ArticlesService;
 
 namespace WodCatClone.Web.Pages.ActionsPage.ArticlesPage
 {
     public partial class ArticlesDetailsPage
     {
         [Parameter] public int ArticleId { get; set; }
+
+        [Inject] public IArticlesService ArticlesService { get; set; }
+
+        public Articles Article { get; set; }
+
+        public string Image { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Article = ArticlesService.GetArticle(ArticleId);
+            Image = ArticlesService.GetImage(Article.ArticlesEmblemId);
+        }
     }
 }
