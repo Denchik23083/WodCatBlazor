56721ef0043d981cd62a8356697aee1c9fa15c57
Denchik23083 <deniskudravov228@gmail.com>
2022-08-02 19:50:15 +0300

    My thesis is done. This is my last commit

--- WodCatClone/WodCatClone.Web/PageComponents/ActionsComponent/EventsComponent/AddEditEventComponent.razor.cs
+++ WodCatClone/WodCatClone.Web/PageComponents/ActionsComponent/EventsComponent/AddEditEventComponent.razor.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using Microsoft.AspNetCore.Components;
 using WodCatClone.Db.Entities.Actions;
 using WodCatClone.Logic.ActionsService.EventsService;
-using WodCatClone.Logic.ActionsService.ExercisesService;
 using WodCatClone.Logic.ActionsService.HallsService;
 using WodCatClone.Logic.ActionsService.WorkoutsService;
 using WodCatClone.Web.Helpers;
