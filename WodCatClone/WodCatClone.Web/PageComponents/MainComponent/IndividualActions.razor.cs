ec0d50ae5c89e905856acbde1c4e49c6acb6161c
Denchik23083 <deniskudravov228@gmail.com>
2023-08-20 16:14:06 +0300

    Main Page is done

--- WodCatClone/WodCatClone.Web/PageComponents/MainComponent/IndividualActions.razor.cs
+++ WodCatClone/WodCatClone.Web/PageComponents/MainComponent/IndividualActions.razor.cs
@@ -1,67 +1,51 @@
 ﻿using Microsoft.AspNetCore.Components;
 using WodCatClone.Db.Entities.Actions;
-using WodCatClone.Logic.ActionsService.ArticlesService;
-using WodCatClone.Logic.ActionsService.ExercisesService;
-using WodCatClone.Logic.ActionsService.HallsService;
-using WodCatClone.Logic.ActionsService.ProgramsService;
-using WodCatClone.Logic.ActionsService.WorkoutsService.WorkoutsService;
 
 namespace WodCatClone.Web.PageComponents.MainComponent
 {
     public partial class IndividualActions
     {
-        [Inject] public IProgramsService ProgramsService { get; set; }
+        [Parameter] public Programs? Program { get; set; }
 
-        [Inject] public IWorkoutsService WorkoutsService { get; set; }
+        [Parameter] public Workouts? Workout { get; set; }
 
-        [Inject] public IExercisesService ExercisesService { get; set; }
+        [Parameter] public Exercises? Exercise { get; set; }
 
-        [Inject] public IHallsService HallsService { get; set; }
+        [Parameter] public Halls? Hall { get; set; }
 
-        [Inject] public IArticlesService ArticlesService { get; set; }
+        [Parameter] public Articles? Article { get; set; }
 
-        [Parameter] public Programs Program { get; set; }
+        public string? ProgramImage { get; set; }
 
-        [Parameter] public Workouts Workout { get; set; }
-
-        [Parameter] public Exercises Exercise { get; set; }
-
-        [Parameter] public Halls Hall { get; set; }
-
-        [Parameter] public Articles Article { get; set; }
-
-        public string ProgramImage { get; set; }
-
-        public string WorkoutImage { get; set; }
+        public string? WorkoutImage { get; set; }
         
-        public string ExerciseImage { get; set; }
+        public string? ExerciseImage { get; set; }
         
-        public string HallImage { get; set; }
+        public string? HallImage { get; set; }
 
-        public string ArticleImage { get; set; }
+        public string? ArticleImage { get; set; }
 
         protected override void OnInitialized()
         {
-            if (Program is not null && Program.ProgramsEmblemId is not null)
+            if (Program?.ProgramEmblem is not null)
             {
-                ProgramImage = ProgramsService.GetImage(Program.ProgramsEmblemId);
+                ProgramImage = Program.ProgramEmblem!.Image;
             }
-            if (Workout is not null && Workout.HallId is not null)
+            if (Workout?.HallId is not null)
             {
-                var hall = HallsService.GetHall(Workout.HallId);
-                WorkoutImage = HallsService.GetImage(hall.EmblemHallId);
+                WorkoutImage = Workout.Halls!.EmblemHall!.Image;
             }
-            if (Exercise is not null)
+            if (Exercise?.Image is not null)
             {
-                //ExerciseImage = ExercisesService.GetExercise(Exercise.Id).Image;
+                ExerciseImage = Exercise.Image;
             }
-            if (Hall is not null)
+            if (Hall?.EmblemHall is not null)
             {
-                HallImage = HallsService.GetImage(Hall.EmblemHallId);
+                HallImage = Hall.EmblemHall!.Image;
             }
-            if (Article is not null && Article.ArticleEmblemId is not null)
+            if (Article?.ArticleEmblem is not null)
             {
-                ArticleImage = ArticlesService.GetImage(Article.ArticleEmblemId);
+                ArticleImage = Article.ArticleEmblem!.Image;
             }
         }
     }
