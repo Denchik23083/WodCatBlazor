daf433e519ae4a3e4de6e5c35c45318e8b7069b0
Denchik23083 <deniskudravov228@gmail.com>
2023-09-08 18:29:12 +0300

    templates tests

---
+++ WodCatClone/WodCatClone.Tests/ActionsTests/ExercisesTests/ExercisesTests.cs
@@ -0,0 +1,15 @@
+﻿using Moq;
+using WodCatClone.WebDb.ActionsRepository.ExercisesRepository;
+
+namespace WodCatClone.Tests.ActionsTests.ExercisesTests
+{
+    public class ExercisesTests
+    {
+        private readonly Mock<IExercisesRepository> _repository;
+
+        public ExercisesTests()
+        {
+            _repository = new Mock<IExercisesRepository>();
+        }
+    }
+}
---
+++ WodCatClone/WodCatClone.Tests/ActionsTests/ProgramsTests/ProgramsTests.cs
@@ -0,0 +1,15 @@
+﻿using Moq;
+using WodCatClone.WebDb.ActionsRepository.ProgramsRepository;
+
+namespace WodCatClone.Tests.ActionsTests.ProgramsTests
+{
+    public class ProgramsTests
+    {
+        private readonly Mock<IProgramsRepository> _repository;
+
+        public ProgramsTests()
+        {
+            _repository = new Mock<IProgramsRepository>();
+        }
+    }
+}
